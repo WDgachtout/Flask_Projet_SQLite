f7fba164aa2c2cc4c6a107436214b129c0a47979
WDgachtout <wdgachtout@gmail.com>
2025-03-18 15:36:31 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,24 @@
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
+CREATE TABLE IF NOT EXISTS books (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    genre TEXT,
+    stock INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role TEXT CHECK(role IN ('user', 'admin')) NOT NULL DEFAULT 'user'
+);
+
+CREATE TABLE IF NOT EXISTS borrows (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    book_id INTEGER NOT NULL,
+    borrow_date DATE DEFAULT (date('now')),
+    return_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (book_id) REFERENCES books (id)
 );
